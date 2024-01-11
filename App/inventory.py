53de96cf5898df89f138cebe813c1c1ad96c1882
Caesar <caesar.keene@gmail.com>
2024-01-11 11:44:42 +0300

    added date and time

--- App/inventory.py
+++ App/inventory.py
@@ -1,2 +1,3 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from datetime import datetime
