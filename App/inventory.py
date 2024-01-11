e06472c592e19f59db8b6e3b182098d959b74a61
Caesar <caesar.keene@gmail.com>
2024-01-11 11:56:12 +0300

    added item class

--- App/inventory.py
+++ App/inventory.py
@@ -5,3 +5,16 @@ import click
 
 Base = declarative_base() 
 
+engine = create_engine('sqlite:///inventory.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    category = relationship('Category', back_populates='items')
+    supplier_id = Column(Integer, ForeignKey('suppliers.id'))
+    supplier = relationship('Supplier', back_populates='supplied_items')
