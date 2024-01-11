e638edf8509787657d45bd0140b72997abc605ec
Caesar <caesar.keene@gmail.com>
2024-01-11 11:46:18 +0300

    imported click

--- App/inventory.py
+++ App/inventory.py
@@ -1,3 +1,4 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 from datetime import datetime
+import click
